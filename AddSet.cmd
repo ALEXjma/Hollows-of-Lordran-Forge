eb3660fd51cc431bce230526513c76f5cacd083c
Alex <joaoalbino04@gmail.com>
2024-04-06 03:19:14 +0100

    AddSet & RemoveSet rework
    No more need to specify where Forge is installed, cardscripts and tokenscripts are now copied to the userDir custom folder instead of the rootDir res folder.

--- AddSet.cmd
+++ AddSet.cmd
@@ -1,37 +1,39 @@
 @echo off
 pushd "%~dp0"
 
-set "stored_forge_location_file=%~dp0.forge_location.txt"
-
-if not exist "%stored_forge_location_file%" (
-    call :input_forge_location
-) else (
-    set /p "forge_location="<"%stored_forge_location_file%"
-)
-call :verify_forge_location
+::set "stored_forge_location_file=%~dp0.forge_location.txt"
+::
+::if not exist "%stored_forge_location_file%" (
+::    call :input_forge_location
+::) else (
+::    set /p "forge_location="<"%stored_forge_location_file%"
+::)
+::call :verify_forge_location
 call :add_set
 goto exit
 
-:input_forge_location
-echo Please specify the path to the Forge instalation directory, example: C:\Games\Forge
-set /p forge_location="-> "
-call :verify_forge_location
-echo %forge_location%>"%stored_forge_location_file%"
-goto :eof
+:::input_forge_location
+::echo Please specify the path to the Forge instalation directory, example: C:\Games\Forge
+::set /p forge_location="-> "
+::call :verify_forge_location
+::echo %forge_location%>"%stored_forge_location_file%"
+::goto :eof
 
-:verify_forge_location
-if not exist "%forge_location%\res" (
-    echo The Forge instalation directory is invalid, please verify the directory contains the "res" folder
-    goto :input_forge_location
-)
-goto :eof
+:::verify_forge_location
+::if not exist "%forge_location%\res" (
+::    echo The Forge instalation directory is invalid, please verify the directory contains the "res" folder
+::    goto :input_forge_location
+::)
+::goto :eof
 
 :add_set
 copy /y "%~dp0editions\Hollows_of_Lordran.txt" "%appdata%\Forge\custom\editions\" >nul 2>&1
 robocopy /e "%~dp0pics\cards" "%localappdata%\Forge\Cache\pics\cards" >nul 2>&1
 robocopy /e "%~dp0pics\tokens" "%localappdata%\Forge\Cache\pics\tokens" >nul 2>&1
-robocopy /e "%~dp0res\cardsfolder" "%forge_location%\res\cardsfolder" >nul 2>&1
-robocopy /e "%~dp0res\tokenscripts" "%forge_location%\res\tokenscripts" >nul 2>&1
+::robocopy /e "%~dp0res\cardsfolder" "%forge_location%\res\cardsfolder" >nul 2>&1
+::robocopy /e "%~dp0res\tokenscripts" "%forge_location%\res\tokenscripts" >nul 2>&1
+robocopy /e "%~dp0res\cardsfolder" "%appdata%\Forge\custom\cards" >nul 2>&1
+robocopy /e "%~dp0res\tokenscripts" "%appdata%\Forge\custom\tokens" >nul 2>&1
 goto :eof
 
 :exit
--- RemoveSet.cmd
+++ RemoveSet.cmd
@@ -1,30 +1,30 @@
 @echo off
 pushd "%~dp0"
 
-set "stored_forge_location_file=%~dp0.forge_location.txt"
-
-if not exist "%stored_forge_location_file%" (
-    call :input_forge_location
-) else (
-    set /p "forge_location="<"%stored_forge_location_file%"
-)
-call :verify_forge_location
+::set "stored_forge_location_file=%~dp0.forge_location.txt"
+::
+::if not exist "%stored_forge_location_file%" (
+::    call :input_forge_location
+::) else (
+::    set /p "forge_location="<"%stored_forge_location_file%"
+::)
+::call :verify_forge_location
 call :remove_set
 goto exit
 
-:input_forge_location
-echo Please specify the path to the Forge instalation directory, example: C:\Games\Forge
-set /p forge_location="-> "
-call :verify_forge_location
-echo %forge_location%>"%stored_forge_location_file%"
-goto :eof
+:::input_forge_location
+::echo Please specify the path to the Forge instalation directory, example: C:\Games\Forge
+::set /p forge_location="-> "
+::call :verify_forge_location
+::echo %forge_location%>"%stored_forge_location_file%"
+::goto :eof
 
-:verify_forge_location
-if not exist "%forge_location%\res" (
-    echo The Forge instalation directory is invalid, please verify the directory contains the "res" folder
-    goto :input_forge_location
-)
-goto :eof
+:::verify_forge_location
+::if not exist "%forge_location%\res" (
+::    echo The Forge instalation directory is invalid, please verify the directory contains the "res" folder
+::    goto :input_forge_location
+::)
+::goto :eof
 
 :remove_set
 del /q "%appdata%\Forge\custom\editions\Hollows_of_Lordran.txt" >nul 2>&1
@@ -32,10 +32,14 @@ rmdir /s /q "%localappdata%\Forge\Cache\pics\cards\HLW" >nul 2>&1
 rmdir /s /q "%localappdata%\Forge\Cache\pics\cards\MHLW" >nul 2>&1
 del /q "%localappdata%\Forge\Cache\pics\tokens\*_hlw.png" >nul 2>&1
 del /q "%localappdata%\Forge\Cache\pics\tokens\*_mhlw.png" >nul 2>&1
-rmdir /s /q "%forge_location%\res\cardsfolder\hlw" >nul 2>&1
-rmdir /s /q "%forge_location%\res\cardsfolder\mhlw" >nul 2>&1
-rmdir /s /q "%forge_location%\res\tokenscripts\hlw" >nul 2>&1
-rmdir /s /q "%forge_location%\res\tokenscripts\mhlw" >nul 2>&1
+::rmdir /s /q "%forge_location%\res\cardsfolder\hlw" >nul 2>&1
+::rmdir /s /q "%forge_location%\res\cardsfolder\mhlw" >nul 2>&1
+::rmdir /s /q "%forge_location%\res\tokenscripts\hlw" >nul 2>&1
+::rmdir /s /q "%forge_location%\res\tokenscripts\mhlw" >nul 2>&1
+rmdir /s /q "%appdata%\Forge\custom\cards\hlw" >nul 2>&1
+rmdir /s /q "%appdata%\Forge\custom\cards\mhlw" >nul 2>&1
+rmdir /s /q "%appdata%\Forge\custom\tokens\hlw" >nul 2>&1
+rmdir /s /q "%appdata%\Forge\custom\tokens\mhlw" >nul 2>&1
 goto :eof
 
 :exit
